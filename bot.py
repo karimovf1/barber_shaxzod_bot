022ea472d8c4d87adec85cd2e055f1af227aa53d
karimovf1 <akromkarimov271@gmail.com>
2025-06-25 00:50:01 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,23 +1,20 @@
 import os
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# .env fayldan tokenni yuklash
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
-# /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("👋 Assalomu alaykum! barber_shaxzod botiga xush kelibsiz.")
 
-# Botni ishga tushurish funksiyasi
 def main():
     if not TOKEN:
-        print("❌ BOT_TOKEN topilmadi! .env faylni tekshiring.")
+        print("❌ BOT_TOKEN topilmadi. .env faylni tekshiring.")
         return
 
-    app = Application.builder().token(TOKEN).build()
+    app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
 
     print("✅ Bot ishga tushdi!")

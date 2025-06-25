a1416830f999227162eb7388a305d0049ce27fa2
karimovf1 <akromkarimov271@gmail.com>
2025-06-25 12:48:21 +0500

    Update bot.py
    Fix for telegram.ext error in PTB 20.7

--- bot.py
+++ bot.py
@@ -1,23 +1,23 @@
+import logging
 import os
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
+# Tokenni yuklaymiz
 load_dotenv()
 TOKEN = os.getenv("BOT_TOKEN")
 
+# Logger sozlamalari
+logging.basicConfig(level=logging.INFO)
+
+# /start komandasi uchun handler
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("👋 Assalomu alaykum! barber_shaxzod botiga xush kelibsiz.")
+    await update.message.reply_text("Assalomu alaykum! Barber Shaxzod botiga xush kelibsiz!")
 
 def main():
-    if not TOKEN:
-        print("❌ BOT_TOKEN topilmadi. .env faylni tekshiring.")
-        return
-
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-
-    print("✅ Bot ishga tushdi!")
     app.run_polling()
 
 if __name__ == "__main__":

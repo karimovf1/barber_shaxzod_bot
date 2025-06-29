ca756e9f3ec42abf874792244206b199c26ebf8f
karimovf1 <akromkarimov271@gmail.com>
2025-06-29 13:50:22 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -155,7 +155,7 @@ if __name__ == '__main__':
     app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^.*(09|10|11|12|13|14|15|16|17|18|19|20|21):00.*$"), choose_time))
     app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^📋 Xizmat turlari$"), handle_services_button))
 
-    app.run_poll
+    app.run_polling()
 
 
 

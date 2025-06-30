b0c0ce20a633dfb0f7cc1f6ba899d37423893422
karimovf1 <akromkarimov271@gmail.com>
2025-06-30 20:05:03 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -81,7 +81,11 @@ async def location(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("📍 Manzilimiz: Toshkent, Sergeli tumani, Xiyobon ko‘chasi 25-uy")
 
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("ℹ️ Yordam: Har qanday savol uchun admin bilan bog‘laning yoki /start buyrug‘ini bosing. Admin lichkasi https://t.me/@barber_shaxzod")
+    async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+        "ℹ️ Yordam: Agar savolingiz bo‘lsa, adminga murojaat qiling:\n\n@barber_shaxzod"
+    )
+
 
 async def book(update: Update, context: ContextTypes.DEFAULT_TYPE):
     context.user_data.clear()

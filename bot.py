456266e1bd7fb29024d990427fc73b4b43f765aa
karimovf1 <akromkarimov271@gmail.com>
2025-06-30 10:41:21 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -189,7 +189,8 @@ async def handle_services_button(update: Update, context: ContextTypes.DEFAULT_T
     await book(update, context)
 
 if __name__ == '__main__':
-    app = ApplicationBuilder().token("TOKEN_BU_YERGA").build()
+   if __name__ == '__main__':
+    app = ApplicationBuilder().token("8112474957:AAHAUjJwLGAku4RJZUKtlgQnB92EEsaIZus").build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("book", book))
